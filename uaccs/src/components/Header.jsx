e6e4ba2a6f6a82499d96c0acef058a1b6bebb975
rohan-projects01 <rohan01.projects@gmail.com>
2026-05-20 02:19:38 -0700

    added imports for two icons

--- uaccs/src/components/Header.jsx
+++ uaccs/src/components/Header.jsx
@@ -1,9 +1,14 @@
+import {Umbrella} from 'lucide-react'
+import {Menu} from 'lucide-react'
 
 /* Header component for Logo, Company Name, Navigation Menu Button */
 function Header() {
 
     return (
         <header className = "relative z-20 w-full px-6 py-6 flex flex-row bg-[#2C3D28] border-b-3 border-[#1F2B1C]">
+            <div>LOGO</div>
+            <div>NAME</div>
+            <button></button>
 
         </header>
     )
