13ac26d85bae5bfa79a23bec50a956e3ccfd9ad6
rohan-projects01 <rohan01.projects@gmail.com>
2026-05-20 04:48:52 -0700

    updated NavigationSidebar to handle close event

--- uaccs/src/App.jsx
+++ uaccs/src/App.jsx
@@ -12,7 +12,7 @@ function App() {
    <div className = "h-screen w-full flex flex-col overflow-hidden">
     <Background/>
     <Header setNavStatus = {setIsNavOpen}/>
-    <NavigationSidebar isOpen = {isNavOpen}/>
+    <NavigationSidebar isOpen = {isNavOpen} onClose = {() => setIsNavOpen(false)}/>
     <Footer/>
    </div>
   )
