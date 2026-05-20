efa4c4ab2e44c6b17e0f8faa7d2ea7b4176d606e
rohan-projects01 <rohan01.projects@gmail.com>
2026-05-20 02:13:15 -0700

    added header component tag to App.jsx

--- uaccs/src/App.jsx
+++ uaccs/src/App.jsx
@@ -1,13 +1,15 @@
 import { useState } from 'react'
 import './index.css'
-import Background from './components/Background';
+import Background from './components/Background'
 import Footer from './components/Footer'
+import Header from './components/Header'
 
 function App() {
     const [count, setCount] = useState(0);
 
   return (
    <div>
+    <Header/>
     <Background/>
     <Footer/>
    </div>
