50c9263995f5a00004910fb80222f55cabd38e04
rohan-projects01 <rohan01.projects@gmail.com>
2026-05-20 02:12:34 -0700

    updated positioning to remove background overlap onto respective components

--- uaccs/src/components/Header.jsx
+++ uaccs/src/components/Header.jsx
@@ -2,7 +2,11 @@
 /* Header component for Logo, Company Name, Navigation Menu Button */
 function Header() {
 
+    return (
+        <header className = "relative z-20 w-full px-6 py-6 flex flex-row bg-[#2C3D28] border-b-3 border-[#1F2B1C]">
 
+        </header>
+    )
 
 
 
