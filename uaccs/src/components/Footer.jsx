c94c37c51fd50e2179ece5da717555cb025bfe2d
rohan-projects01 <rohan01.projects@gmail.com>
2026-05-20 01:25:57 -0700

    updated footer information displayed

--- uaccs/src/components/Footer.jsx
+++ uaccs/src/components/Footer.jsx
@@ -4,9 +4,9 @@ function Footer() {
 
     return (
         <footer className = "absolute w-full px-6 py-6 mt-auto bg-[#3A4A33] border-t-4 border-[#2B3625]">
-            
+
             <div className = "max-w-7xl mx-auto text-center text-[#B8C5B0] text-[14px]">
-              &copy; {new Date().getFullYear()} Ucluelet and Area Childcare Society. All Rights Reserved | ADDRESS | PHONE NUMBER
+              &copy; {new Date().getFullYear()} Ucluelet and Area Childcare Society. All Rights Reserved | 500 Matterson Drive, V0R 3A0  | (250) 726-2339
             </div>
 
         </footer>
