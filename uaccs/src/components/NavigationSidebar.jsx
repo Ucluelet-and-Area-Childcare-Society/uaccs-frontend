000bb749c798f6a9dfdf46a2b252575bd7a1a32d
rohan-projects01 <rohan01.projects@gmail.com>
2026-05-20 05:18:18 -0700

    created array navOptions to map over as navigation choices in the NavigationSidebar

--- uaccs/src/components/NavigationSidebar.jsx
+++ uaccs/src/components/NavigationSidebar.jsx
@@ -1,7 +1,18 @@
-import {X, House, Users, Phone, Calendar, Info, Heart, ClipboardList} from 'lucide-react'
+import {X, House, Users, Phone, Calendar, Info, Heart, ClipboardList, Book} from 'lucide-react'
 
 /* NavigationSidebar component to enable navigation between web pages */
 function NavigationSidebar({isOpen, onClose}) {
+    // array of options to map over to create NavigationBar
+    const navOptions = [
+        {icon: House, label: "Home"},
+        {icon: Info, label: "Who We Are"},
+        {icon: ClipboardList, label: "Waitlist"},
+        {icon: Users, label: "Our Programs"},
+        {icon: Heart, label: "Get Involved"},
+        {icon: Book, label: "Resources"},
+        {icon: Calendar, label: "Calendar"},
+        {icon: Phone, label: "Conact"},
+    ]
     
     return (
        <>
