aab381d44c999aeab3e41528c87b66b7efd97acc
rohan-projects01 <rohan01.projects@gmail.com>
2026-05-19 20:54:15 -0700

    created tan/beige background underlayed to text

--- uaccs/src/components/Background.jsx
+++ uaccs/src/components/Background.jsx
@@ -3,6 +3,14 @@
 /* Background component for randomly layed out phrase as a watermark */
 function Background() {
 
+    return (
+        <div class = "min-h-screen bg-[#F7F5F0]">
+
+
+
+        </div>
+    )
+
 }
 
 export default Background;
