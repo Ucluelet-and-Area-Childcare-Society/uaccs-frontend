2032e322f1bd2a829e25bf8bd105cf87452144ad
rohan-projects01 <rohan01.projects@gmail.com>
2026-05-20 02:42:24 -0700

    added image icon on left of header component

--- uaccs/src/components/Header.jsx
+++ uaccs/src/components/Header.jsx
@@ -6,9 +6,7 @@ function Header() {
 
     return (
         <header className = "relative z-20 w-full px-6 py-6 flex flex-row bg-[#2C3D28] border-b-3 border-[#1F2B1C]">
-            <img src = {Umbrella} alt = "Umbrella Logo"/>
-            <div>NAME</div>
-            <Menu size = {28}/>
+            <img src = {Umbrella} alt = "Umbrella Logo" className= "w-16 h-16"/>
 
         </header>
     )
