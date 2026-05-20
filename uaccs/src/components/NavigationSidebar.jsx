2c3acef83ec85c94ad045f7cfc938018b060fbbf
rohan-projects01 <rohan01.projects@gmail.com>
2026-05-20 04:49:41 -0700

    updated NavigationSidebar() to take onClose parameter

--- uaccs/src/components/NavigationSidebar.jsx
+++ uaccs/src/components/NavigationSidebar.jsx
@@ -1,7 +1,11 @@
 
 /* NavigationSidebar component to enable navigation between web pages */
-function NavigationSidebar({isOpen}) {
-
+function NavigationSidebar({isOpen, onClose}) {
+    
+    return (
+       <>
+       </> 
+    )
 }
 
 export default NavigationSidebar;
