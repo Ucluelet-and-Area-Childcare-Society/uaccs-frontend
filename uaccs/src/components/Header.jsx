ac56a0b50759eb763668e26be7affa240c6fe073
rohan-projects01 <rohan01.projects@gmail.com>
2026-05-20 02:58:37 -0700

    updated logo name structure in Header() component

--- uaccs/src/components/Header.jsx
+++ uaccs/src/components/Header.jsx
@@ -7,7 +7,14 @@ function Header() {
     return (
         <header className = "relative z-20 w-full px-6 py-6 flex flex-row bg-[#2C3D28] border-b-3 border-[#1F2B1C]">
             <img src = {Umbrella} alt = "Umbrella Logo" className= "w-16 h-16"/>
-            
+            <div>
+                <div>
+                    Ucluelet and Area
+                </div>
+                <div>
+                    Childcare Society
+                </div>
+            </div>
 
         </header>
     )
