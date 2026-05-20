2f11f6c1b98f61b533c5769e2d5466c2a746b53c
rohan-projects01 <rohan01.projects@gmail.com>
2026-05-19 21:10:23 -0700

    testing out Background component

--- uaccs/src/App.jsx
+++ uaccs/src/App.jsx
@@ -1,11 +1,14 @@
 import { useState } from 'react'
 import './index.css'
+import Background from './components/Background';
 
 function App() {
     const [count, setCount] = useState(0);
 
   return (
-    <h1>UACCS Frontend</h1>
+   <div>
+        <Background />
+   </div>
   )
 }
 
