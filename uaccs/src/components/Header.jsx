569a1f521bde372ae903aa4c063f052c72e4ee64
rohan-projects01 <rohan01.projects@gmail.com>
2026-05-20 02:40:50 -0700

    imported svg for umbrella icon

--- uaccs/src/components/Header.jsx
+++ uaccs/src/components/Header.jsx
@@ -1,14 +1,14 @@
-import {Umbrella} from 'lucide-react'
 import {Menu} from 'lucide-react'
+import Umbrella from '../assets/umbrella-logo.svg'
 
 /* Header component for Logo, Company Name, Navigation Menu Button */
 function Header() {
 
     return (
         <header className = "relative z-20 w-full px-6 py-6 flex flex-row bg-[#2C3D28] border-b-3 border-[#1F2B1C]">
-            <div>LOGO</div>
+            <img src = {Umbrella} alt = "Umbrella Logo"/>
             <div>NAME</div>
-            <button></button>
+            <Menu size = {28}/>
 
         </header>
     )
