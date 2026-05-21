af35db8e89202431ded31f699cad874bba36f4cd
rohan-projects01 <rohan01.projects@gmail.com>
2026-05-20 18:32:15 -0700

    updated color scheme on NavigationSidebar

--- uaccs/src/components/NavigationSidebar.jsx
+++ uaccs/src/components/NavigationSidebar.jsx
@@ -20,13 +20,16 @@ function NavigationSidebar({isOpen, onClose}) {
 
         <div 
         className={`${isOpen ? "translate-x-0" : "translate-x-full"} fixed top-0 
-        right-0 h-full bg-[#2C3D28] w-96 z-50 transition duration-300 ease-in-out border-l-4 border-solid border-[#C9B896]
+        right-0 h-full bg-[#C4D65E] w-96 z-50 transition duration-300 ease-in-out border-l-4 border-solid border-[#849236]
         shadow-[-8px_0_24px_rgba(0,0,0,0.3)] shadow-black/30`}>
 
             <div className = 'flex justify-between items-center border-b-2 border-[#C9B896] bg-linear-to-b from-[#2C3D28] from-0% to-[#253324] to-100%'>
                 <div className='tracking-wider text-[32px] font-dancing text-[#C9B896] font-light'>
                     Menu
                 </div>
+                <button onClick = {onClose}>
+                    <X size = {28} strokeWidth = {2.5}/>
+                </button>
             </div>
 
         </div>
