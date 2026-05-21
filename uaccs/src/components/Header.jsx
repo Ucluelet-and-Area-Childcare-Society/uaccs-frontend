f0e09bebce3a1e9c788aa9628226e5b01d8cc575
rohan-projects01 <rohan01.projects@gmail.com>
2026-05-20 17:53:49 -0700

    updated color scheme on header

--- uaccs/src/components/Header.jsx
+++ uaccs/src/components/Header.jsx
@@ -5,14 +5,14 @@ import Umbrella from '../assets/umbrella-logo.svg'
 function Header({setNavStatus}) {
 
     return (
-        <header className = "relative z-20 w-full px-6 py-4 flex flex-row bg-[#2C3D28] border-b-3 border-[#1F2B1C] justify-between">
+        <header className = "relative z-20 w-full px-6 py-4 flex flex-row bg-[#4A8A9A] border-b-3 border-[#3A6A77] justify-between">
             <div className='flex items-center gap-3 select-none'>
                 <img src = {Umbrella} alt = "Umbrella Logo" className= "w-16 h-16"/>
                 <div className='tracking-wider'>
                     <div className='text-[18px] text-white font-bold'>
                         UCLELET &amp; AREA
                     </div>
-                    <div className='text-[11px] text-[#C9B896] font-semibold'>
+                    <div className='text-[11px] text-[#C4D65E] font-semibold'>
                         CHILDCARE SOCIETY
                     </div>
                 </div>
