f6c9a8466418fb67645acdff4f7b008565aefbe6
rohan-projects01 <rohan01.projects@gmail.com>
2026-05-21 18:57:25 -0700

    removed unused import

--- uaccs/src/components/Header.jsx
+++ uaccs/src/components/Header.jsx
@@ -1,5 +1,4 @@
-import {Menu} from 'lucide-react'
-import Umbrella from '../assets/umbrella-logo.svg'
+import Umbrella from '../assets/file-2.svg'
 import {NavLink} from 'react-router'
 
 /* Header component for Logo, Company Name, Navigation Menu Button */
@@ -8,7 +7,7 @@ function Header() {
     return (
         <header className = "relative z-20 w-full px-6 py-4 flex flex-row bg-[#4A8A9A] border-b-3 border-[#3A6A77] justify-between">
             <div className='flex items-center gap-3 select-none'>
-                <img src = {Umbrella} alt = "Umbrella Logo" className= "w-16 h-16"/>
+                <img src = {Umbrella} alt = "Umbrella Logo" className= "w-24 h-24 "/>
                 <div className='tracking-wider'>
                     <div className='text-[18px] text-white font-bold'>
                         UCLUELET &amp; AREA
@@ -18,6 +17,9 @@ function Header() {
                     </div>
                 </div>
             </div>
+            <nav>
+                
+            </nav>
 
         </header>
     )
