c6d57fb487be5c3a2a364cd9b5fea35efc8108c5
rohan-projects01 <rohan01.projects@gmail.com>
2026-05-21 19:01:06 -0700

    logo change

--- uaccs/src/components/Header.jsx
+++ uaccs/src/components/Header.jsx
@@ -1,4 +1,4 @@
-import Umbrella from '../assets/file-2.svg'
+import Umbrella from '../assets/umbrella-logo.svg'
 import {NavLink} from 'react-router'
 
 /* Header component for Logo, Company Name, Navigation Menu Button */
@@ -7,7 +7,7 @@ function Header() {
     return (
         <header className = "relative z-20 w-full px-6 py-4 flex flex-row bg-[#4A8A9A] border-b-3 border-[#3A6A77] justify-between">
             <div className='flex items-center gap-3 select-none'>
-                <img src = {Umbrella} alt = "Umbrella Logo" className= "w-24 h-24 "/>
+                <img src = {Umbrella} alt = "Umbrella Logo" className= "w-16 h-16 "/>
                 <div className='tracking-wider'>
                     <div className='text-[18px] text-white font-bold'>
                         UCLUELET &amp; AREA
