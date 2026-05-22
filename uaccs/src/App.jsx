0b8c8afe85c7e05380bfbce37c88c6a02340793f
rohan-projects01 <rohan01.projects@gmail.com>
2026-05-21 17:59:33 -0700

    set up react router navigation for home page

--- uaccs/src/App.jsx
+++ uaccs/src/App.jsx
@@ -3,6 +3,8 @@ import './index.css'
 import Background from './components/Background'
 import Footer from './components/Footer'
 import Header from './components/Header'
+import {Routes, Route} from 'react-router'
+import Home from './pages/Home'
 
 function App() {
     const [isNavOpen, setIsNavOpen] = useState(false); // setting NavBar to closed initially
@@ -11,6 +13,11 @@ function App() {
    <div className = "h-screen w-full flex flex-col overflow-hidden">
     <Background/>
     <Header setNavStatus = {setIsNavOpen}/>
+    <Routes>
+        <Route path = "/" element = {<Home/>}/>
+        
+
+    </Routes>
     <Footer/>
    </div>
   )
