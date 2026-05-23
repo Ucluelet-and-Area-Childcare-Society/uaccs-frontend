3e23b1b194e6ff11b4d8a139e4180623962a5125
rohan-projects01 <rohan01.projects@gmail.com>
2026-05-23 02:11:06 -0700

    imported chevrondown icon

--- uaccs/src/components/DropdownMenu.jsx
+++ uaccs/src/components/DropdownMenu.jsx
@@ -1,5 +1,6 @@
 import {NavLink} from 'react-router-dom'
 import {Menu, MenuButton, MenuItem, MenuItems} from '@headlessui/react'
+import {ChevronDown} from 'lucide-react'
 
 /* DropdownMenu component, takes in a label & array of dropdown items */
 function DropdownMenu({label, items}) {
@@ -7,11 +8,11 @@ function DropdownMenu({label, items}) {
     return (
         <Menu>
             <MenuButton className = 'transition-all duration-300 ease-in-out hover:text-[#C4D65E] hover:-translate-y-1'>About</MenuButton>
+            <MenuItems>
             
-
-
-
-
+            
+            
+            </MenuItems>
         </Menu>
     )
 
