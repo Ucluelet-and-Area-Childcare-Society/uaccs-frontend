21ccaef239e737b30c1aa1e8fed91d51a5f8abaf
rohan-projects01 <rohan01.projects@gmail.com>
2026-05-23 04:11:52 -0700

    set additional styling to dropdown

--- uaccs/src/components/DropdownMenu.jsx
+++ uaccs/src/components/DropdownMenu.jsx
@@ -7,12 +7,19 @@ function DropdownMenu({label, items}) {
 
     return (
         <Menu.Root>
-            <Menu.Trigger openOnHover className = "flex items-center gap-1 hover:text-[#C4D65E]">
+            <Menu.Trigger openOnHover delay = {100} className = "flex items-center gap-1 hover:text-uaccs_green pointer-events-none">
                 {label}
                 <ChevronDown size={18}/>
             </Menu.Trigger>
-
-
+            <Menu.Portal>
+                <Menu.Positioner side = "bottom" align = "start" sideOffset={4} className = "z-50" >
+                    <Menu.Popup className = "shadow-md bg-white border-2 p-3">
+                        {items.map(item => (
+                            <Menu.Item className = "data-highlighted:opacity-50">{item.label}</Menu.Item>
+                        ))}
+                    </Menu.Popup>
+                </Menu.Positioner>
+            </Menu.Portal>
         </Menu.Root>
     )
 
