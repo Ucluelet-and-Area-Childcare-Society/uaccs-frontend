6fcc30a46fa0cd1ef390f570e714101bdabc1fa6
rohan-projects01 <rohan01.projects@gmail.com>
2026-05-23 00:21:02 -0700

    removed mantine

--- uaccs/src/components/DropdownMenu.jsx
+++ uaccs/src/components/DropdownMenu.jsx
@@ -1,26 +1,13 @@
-import {Menu, Button} from '@mantine/core'
+
 import {NavLink} from 'react-router-dom'
 
 /* DropdownMenu component, takes in a label & array of dropdown items */
 function DropdownMenu({label, items}) {
 
     return (
-        <Menu shadow = "md" width = {200}>
-
-            <Menu.Target>
-                <Button>{label}</Button>
-            </Menu.Target>
-
-            <Menu.Dropdown>
-                {items.map(item => (
-                    <Menu.Item component= {NavLink} to = {item.to} key = {item.to}> 
-                        {item.label}
-                    </Menu.Item>
-                ))}
-                
-            </Menu.Dropdown>
-
-        </Menu>
+        <>
+        </>
+       
     )
 
 }
