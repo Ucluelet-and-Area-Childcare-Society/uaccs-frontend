c61f6e83a5c91d5c08ffd039cdac12f6e4b2b0ce
rohan-projects01 <rohan01.projects@gmail.com>
2026-05-22 22:42:19 -0700

    removed uneeded iconography

--- uaccs/src/components/Header.jsx
+++ uaccs/src/components/Header.jsx
@@ -1,16 +1,15 @@
 import Umbrella from '../assets/umbrella-logo.svg'
 import {NavLink} from 'react-router'
-import {Info, Users, Phone, Heart, Book} from 'lucide-react'
 
 /* Header component for Logo, Company Name, Navigation Menu Button */
 function Header() {
 
     const navLinks = [
-        { to: "/about", label: "About", icon: Info },
-        { to: "/programs", label: "Programs", icon: Users },
-        { to: "/resources", label: "Resources", icon: Book },
-        { to: "/GetInvolved", label: "Get Involved", icon: Heart },
-        { to: "/contact", label: "Contact", icon: Phone },
+        { to: "/about", label: "About"},
+        { to: "/programs", label: "Programs"},
+        { to: "/resources", label: "Resources"},
+        { to: "/GetInvolved", label: "Get Involved"},
+        { to: "/contact", label: "Contact"},
     ]
 
     return (
