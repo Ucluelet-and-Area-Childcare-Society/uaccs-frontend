1d4fc466018fcb31c14443127f593ef9d2e9f490
rohan-projects01 <rohan01.projects@gmail.com>
2026-05-23 03:41:30 -0700

    updated styles to use newly created themes

--- uaccs/src/components/Header.jsx
+++ uaccs/src/components/Header.jsx
@@ -20,14 +20,14 @@ function Header() {
     const programsLinks = []
 
     return (
-        <header className = "relative z-20 w-full px-6 py-4 flex flex-row bg-[#4A8A9A] border-b-3 border-[#3A6A77] justify-between">
+        <header className = "relative z-20 w-full px-6 py-4 flex flex-row bg-uaccs_blue border-b-3 border-[#3A6A77] justify-between">
             <div className='flex items-center gap-3 select-none'>
                 <img src = {Umbrella} alt = "Umbrella Logo" className= "w-16 h-16 "/>
                 <div className='tracking-wider'>
                     <div className='text-[18px] text-white font-bold'>
                         UCLUELET &amp; AREA
                     </div>
-                    <div className='text-[11px] text-[#C4D65E] font-semibold'>
+                    <div className='text-[11px] text-uaccs_green font-semibold'>
                         CHILDCARE SOCIETY
                     </div>
                 </div>
@@ -35,9 +35,9 @@ function Header() {
             <nav className = "flex items-center gap-5 text-white">
                 <DropdownMenu label = "About" items = {aboutLinks} />
                 {navLinks.map((link) => (
-                    <NavLink key = {link.to} to = {link.to} className = 'transition-all duration-300 ease-in-out hover:text-[#C4D65E] hover:-translate-y-1'> {link.label} </NavLink>
+                    <NavLink key = {link.to} to = {link.to} className = 'transition-all duration-300 ease-in-out hover:text-uaccs_green hover:-translate-y-1'> {link.label} </NavLink>
                 ))}
-                <NavLink to = "/waitlist" className = 'bg-[#E8A562] p-2 rounded-md hover:text-[#E8A562] hover:bg-white transition-all duration-300 ease-in-out hover:-translate-y-1'> Waitlist </NavLink>
+                <NavLink to = "/waitlist" className = 'bg-uaccs_orange p-2 rounded-md hover:text-uaccs_orange hover:bg-white transition-all duration-300 ease-in-out hover:-translate-y-1'> Waitlist </NavLink>
             </nav>
 
         </header>
