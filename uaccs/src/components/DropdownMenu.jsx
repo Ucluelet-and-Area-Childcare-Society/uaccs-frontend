66cb3929581c24898a04bba5156cddb0b63359ce
rohan-projects01 <rohan01.projects@gmail.com>
2026-05-23 03:36:12 -0700

    added menu stop using baseui

--- uaccs/src/components/DropdownMenu.jsx
+++ uaccs/src/components/DropdownMenu.jsx
@@ -1,12 +1,16 @@
 import {NavLink} from 'react-router-dom'
-
+import {Menu} from '@base-ui/react/menu'
 import {ChevronDown} from 'lucide-react'
 
 /* DropdownMenu component, takes in a label & array of dropdown items */
 function DropdownMenu({label, items}) {
 
     return (
-        
+        <Menu.Root>
+
+
+
+        </Menu.Root>
     )
 
 }
