ef66c32a4b07e5b7c83b9208dd3fc40416cb9143
rohan-projects01 <rohan01.projects@gmail.com>
2026-05-23 03:34:46 -0700

    removed headlessui

--- uaccs/src/components/DropdownMenu.jsx
+++ uaccs/src/components/DropdownMenu.jsx
@@ -1,19 +1,12 @@
 import {NavLink} from 'react-router-dom'
-import {Menu, MenuButton, MenuItem, MenuItems} from '@headlessui/react'
+
 import {ChevronDown} from 'lucide-react'
 
 /* DropdownMenu component, takes in a label & array of dropdown items */
 function DropdownMenu({label, items}) {
 
     return (
-        <Menu>
-            <MenuButton className = 'transition-all duration-300 ease-in-out hover:text-[#C4D65E] hover:-translate-y-1'>About</MenuButton>
-            <MenuItems>
-            
-            
-            
-            </MenuItems>
-        </Menu>
+        
     )
 
 }
