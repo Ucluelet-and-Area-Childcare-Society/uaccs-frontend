59aa41d484371e2d9a1c048ad478ee3643e508ad
rohan-projects01 <rohan01.projects@gmail.com>
2026-05-23 04:21:44 -0700

    added new styling + routing using Linkitems for dropdown

--- uaccs/src/components/DropdownMenu.jsx
+++ uaccs/src/components/DropdownMenu.jsx
@@ -7,7 +7,7 @@ function DropdownMenu({label, items}) {
 
     return (
         <Menu.Root>
-            <Menu.Trigger openOnHover delay = {100} className = "flex items-center gap-1 hover:text-uaccs_green pointer-events-none">
+            <Menu.Trigger openOnHover delay = {100} className = "flex items-center gap-1 hover:text-uaccs_green ">
                 {label}
                 <ChevronDown size={18}/>
             </Menu.Trigger>
@@ -15,7 +15,9 @@ function DropdownMenu({label, items}) {
                 <Menu.Positioner side = "bottom" align = "start" sideOffset={4} className = "z-50" >
                     <Menu.Popup className = "shadow-md bg-white border-2 p-3">
                         {items.map(item => (
-                            <Menu.Item className = "data-highlighted:opacity-50">{item.label}</Menu.Item>
+                            <Menu.LinkItem className = "flex flex-col data-highlighted:opacity-50" render = {<NavLink to = {item.to} /> }>
+                                {item.label}
+                            </Menu.LinkItem>
                         ))}
                     </Menu.Popup>
                 </Menu.Positioner>
