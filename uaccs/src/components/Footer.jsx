5ffb34802452ee0b2197ee58c988dcc75b8ff392
rohan-projects01 <rohan01.projects@gmail.com>
2026-05-23 20:26:50 -0700

    updated color scheme to use newly created theme

--- uaccs/src/components/Footer.jsx
+++ uaccs/src/components/Footer.jsx
@@ -3,7 +3,7 @@
 function Footer() {
 
     return (
-        <footer className = "relative z-10 w-full px-6 py-4 mt-auto bg-[#E8A562] border-t-4 border-[#D89350]">
+        <footer className = "relative z-10 w-full px-6 py-4 mt-auto bg-uaccs_orange border-t-4 border-[#D89350]">
 
             <div className = "max-w-7xl mx-auto text-center text-white text-[14px]">
               &copy; {new Date().getFullYear()} Ucluelet and Area Childcare Society - All Rights Reserved. | 500 Matterson Drive, V0R 3A0  | (250) 726-2339
