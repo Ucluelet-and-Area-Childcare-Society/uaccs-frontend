21f1d3d1b030b7427e9e93bc15addbb6b056a678
rohan-projects01 <rohan01.projects@gmail.com>
2026-05-23 20:12:02 -0700

    set placeholder image with border and sizing

--- uaccs/src/pages/Home.jsx
+++ uaccs/src/pages/Home.jsx
@@ -3,15 +3,22 @@
 function Home() {
 
     return (
-       <>
-        <h1 className = "z-0 font-dancing font-bold text-[#A8BA4E] text-4xl text-center mt-15">
+       <div className = "flex flex-col items-start p-10 mx-auto">
+        <h1 className = "z-0 font-dancing font-bold text-[#A8BA4E] text-4xl">
             Committed to providing affordable, inclusive, safe, and nurturing childcare.
         </h1>
+        <img 
+        src = "/test_img.jpg" 
+        alt = "test image" 
+        width = "500" 
+        height = "500" 
+        className = "mt-20 z-0 border-5 border-uaccs_green outline-2 outline-[#5BA4B5] outline-offset-10"
+        />
        
        
        
        
-       </>
+       </div>
     )
 
 }
