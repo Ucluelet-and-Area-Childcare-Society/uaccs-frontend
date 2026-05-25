8de0ec4c556c7f4acb9dafdb0ec7b69e55bd3c5a
rohan-projects01 <rohan01.projects@gmail.com>
2026-05-25 03:02:22 -0700

    defined items array to map over vertical timeline element

--- uaccs/src/pages/about/WhoWeAre.jsx
+++ uaccs/src/pages/about/WhoWeAre.jsx
@@ -4,6 +4,10 @@ import 'react-vertical-timeline-component/style.min.css'
 import {Compass} from 'lucide-react'
 
 function WhoWeAre() {
+
+    const items = [
+        {title: "Our Mission & Purpose", icon: Compass, border_icon_Color: "#5BA4B5", text: "some text here"}
+    ]
    
     return (
         <VerticalTimeline lineColor='#5BA4B5'>
@@ -14,13 +18,18 @@ function WhoWeAre() {
             contentArrowStyle={{
                 display: 'none'
             }}
+            icon = {<Compass/>}
+            iconStyle={{
+                background: '#FFFFFF',
+                color: '#5BA4B5',
+
+            }}
             >
                 <h3 className = "vertical-timeline-element-title font-dancing text-2xl text-semibold">Our Mission & Purpose</h3>
                 <p>
                     Some paragraph content here...
                 </p>
             </VerticalTimelineElement>
-
 
 
 
