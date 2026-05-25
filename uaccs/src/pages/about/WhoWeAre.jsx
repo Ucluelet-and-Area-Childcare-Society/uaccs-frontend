9b4053379119a3c3006cd168c07c9e0a5ad074c5
rohan-projects01 <rohan01.projects@gmail.com>
2026-05-25 03:30:49 -0700

    added import for timeline items

--- uaccs/src/pages/about/WhoWeAre.jsx
+++ uaccs/src/pages/about/WhoWeAre.jsx
@@ -1,17 +1,8 @@
 import {VerticalTimeline, VerticalTimelineElement} from 'react-vertical-timeline-component'
 import 'react-vertical-timeline-component/style.min.css'
-import {Compass, Telescope, Award, Flag, Lightbulb, Scale} from 'lucide-react'
-
+import {TimelineItems} from '../../data/TimelineItems'
 function WhoWeAre() {
 
-    const items = [
-        {title: "Our Mission & Purpose", icon: Compass, border_icon_color: "#5BA4B5", text: "some text here"},
-        {title: "Vision For The Future", icon: Telescope, border_icon_color: "#E8A562", text: "some more text"},
-        {title: 'Unique Strengths & Capabilities', icon: Award, border_icon_color: "#C4D65E", text: "yet more text..."},
-        {title: "Measuring Success", icon: Flag, border_icon_color: "#5BA4B5", text: "more text,,,"},
-        {title: "Adaptation & Innovation", icon: Lightbulb, border_icon_color: "#E8A562", text: "bleh bleh bleh"},
-        {title: "Ethical Considerations", icon: Scale, border_icon_color: "#C4D65E", text: "again again again"}
-    ]
    
     return (
         <VerticalTimeline lineColor='#5BA4B5'>
@@ -39,12 +30,6 @@ function WhoWeAre() {
 
                 </VerticalTimelineElement>
             ))}
-
-
-
-
-
-
         </VerticalTimeline> 
     )
 
