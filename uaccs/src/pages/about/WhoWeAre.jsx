4edd8ed2aa1269c4db080bb61c35cbcc8153337f
rohan-projects01 <rohan01.projects@gmail.com>
2026-05-25 03:10:41 -0700

    updated vertical timeline component to use a map function and array

--- uaccs/src/pages/about/WhoWeAre.jsx
+++ uaccs/src/pages/about/WhoWeAre.jsx
@@ -6,30 +6,35 @@ import {Compass} from 'lucide-react'
 function WhoWeAre() {
 
     const items = [
-        {title: "Our Mission & Purpose", icon: Compass, border_icon_Color: "#5BA4B5", text: "some text here"}
+        {title: "Our Mission & Purpose", icon: Compass, border_icon_color: "#5BA4B5", text: "some text here"}
     ]
    
     return (
         <VerticalTimeline lineColor='#5BA4B5'>
-            <VerticalTimelineElement
-            contentStyle = {{
-                border: '8px double #5BA4B5'
-            }}
-            contentArrowStyle={{
+            {items.map((item, index) => (
+                <VerticalTimelineElement
+                key = {index}
+                contentStyle = {{
+                    border: `8px double ${item.border_icon_color}`
+                }}
+                contentArrowStyle={{
                 display: 'none'
-            }}
-            icon = {<Compass/>}
-            iconStyle={{
+                }}
+                icon = {<item.icon />}
+                iconStyle={{
                 background: '#FFFFFF',
-                color: '#5BA4B5',
+                color: `${item.border_icon_color}`,
+                }}
+                >
+                    <h3 className = "vertical-timeline-element-title font-dancing text-2xl text-semibold">
+                        {item.title}
+                    </h3>
+                    <p>
+                        {item.text}
+                    </p>
 
-            }}
-            >
-                <h3 className = "vertical-timeline-element-title font-dancing text-2xl text-semibold">Our Mission & Purpose</h3>
-                <p>
-                    Some paragraph content here...
-                </p>
-            </VerticalTimelineElement>
+                </VerticalTimelineElement>
+            ))}
 
 
 
