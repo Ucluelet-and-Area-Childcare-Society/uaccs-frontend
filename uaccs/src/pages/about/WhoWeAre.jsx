f1c943ff24bc4421a5ae2a7c7987fe0a63acbbd2
rohan-projects01 <rohan01.projects@gmail.com>
2026-05-25 02:52:59 -0700

    added new import for compass icon and defined structure of timeline element

--- uaccs/src/pages/about/WhoWeAre.jsx
+++ uaccs/src/pages/about/WhoWeAre.jsx
@@ -1,11 +1,25 @@
 import Timeline from '../../components/Timeline'
 import {VerticalTimeline, VerticalTimelineElement} from 'react-vertical-timeline-component'
 import 'react-vertical-timeline-component/style.min.css'
+import {Compass} from 'lucide-react'
 
 function WhoWeAre() {
    
     return (
-        <VerticalTimeline>
+        <VerticalTimeline lineColor='#5BA4B5'>
+            <VerticalTimelineElement
+            contentStyle = {{
+                border: '8px double #5BA4B5'
+            }}
+            contentArrowStyle={{
+                display: 'none'
+            }}
+            >
+                <h3 className = "vertical-timeline-element-title font-dancing text-2xl text-semibold">Our Mission & Purpose</h3>
+                <p>
+                    Some paragraph content here...
+                </p>
+            </VerticalTimelineElement>
 
 
 
