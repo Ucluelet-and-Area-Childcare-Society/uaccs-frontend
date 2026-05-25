fbf88baeab6c4dd2ba96a470bf45c18e4d6800de
rohan-projects01 <rohan01.projects@gmail.com>
2026-05-25 03:46:52 -0700

    added title to page

--- uaccs/src/pages/about/WhoWeAre.jsx
+++ uaccs/src/pages/about/WhoWeAre.jsx
@@ -6,32 +6,34 @@ import {TimelineItems} from '../../data/TimelineItems'
 function WhoWeAre() {
 
     return (
-        <VerticalTimeline lineColor='#5BA4B5'>
-            {TimelineItems.map((item, index) => (
-                <VerticalTimelineElement
-                key = {index}
-                contentStyle = {{
-                    border: `8px double ${item.border_icon_color}`
-                }}
-                contentArrowStyle={{
-                display: 'none'
-                }}
-                icon = {<item.icon />}
-                iconStyle={{
-                background: '#FFFFFF',
-                color: `${item.border_icon_color}`,
-                }}
-                >
-                    <h3 className = "vertical-timeline-element-title font-dancing text-3xl text-semi mb-10!">
-                        {item.title}
-                    </h3>
-                    <p className = "text-justify text-gray-700">
-                        {item.text}
-                    </p>
-
-                </VerticalTimelineElement>
-            ))}
-        </VerticalTimeline> 
+        <div className = "flex flex-col items-center p-10 mx-auto z-0 w-full">
+            <h1 className = "font-dancing text-[#A8BA4E] text-[100px]">Who We Are</h1>
+            <VerticalTimeline lineColor='#5BA4B5'>
+                {TimelineItems.map((item, index) => (
+                    <VerticalTimelineElement
+                    key = {index}
+                    contentStyle = {{
+                        border: `8px double ${item.border_icon_color}`
+                    }}
+                    contentArrowStyle={{
+                    display: 'none'
+                    }}
+                    icon = {<item.icon />}
+                    iconStyle={{
+                    background: '#FFFFFF',
+                    color: `${item.border_icon_color}`,
+                    }}
+                    >
+                        <h3 className = "vertical-timeline-element-title font-dancing text-3xl text-semi mb-10!">
+                            {item.title}
+                        </h3>
+                        <p className = "text-justify text-gray-700">
+                            {item.text}
+                        </p>
+                    </VerticalTimelineElement>
+                ))}
+            </VerticalTimeline> 
+        </div>
     )
 
 }
