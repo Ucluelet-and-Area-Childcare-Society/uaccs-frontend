cd0e7044cd5c2291efad4c31c0f721bbd7242355
rohan-projects01 <rohan01.projects@gmail.com>
2026-05-25 02:21:42 -0700

    added placeholder items array for new timeline component

--- uaccs/src/pages/about/WhoWeAre.jsx
+++ uaccs/src/pages/about/WhoWeAre.jsx
@@ -1,11 +1,15 @@
-
+import {Chrono} from 'react-chrono'
 
 function WhoWeAre() {
 
+    const items = [
+        {title: 'title', cardTitle: 'cardTitle', cardDetailedText: 'text text'},
+        {title: 'title', cardTitle: 'cardTitle', cardDetailedText: 'text text'}
+    ]
+
    
     return (
-       
-
+        
     )
 
 }
