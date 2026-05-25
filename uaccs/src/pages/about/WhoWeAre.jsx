81349c1b4027e9072bac1f55d7d305cbbe763b4c
rohan-projects01 <rohan01.projects@gmail.com>
2026-05-25 03:26:05 -0700

    added all who we are subelemeents into vertical timeline

--- uaccs/src/pages/about/WhoWeAre.jsx
+++ uaccs/src/pages/about/WhoWeAre.jsx
@@ -1,6 +1,6 @@
 import {VerticalTimeline, VerticalTimelineElement} from 'react-vertical-timeline-component'
 import 'react-vertical-timeline-component/style.min.css'
-import {Compass, Telescope, Award, Flag} from 'lucide-react'
+import {Compass, Telescope, Award, Flag, Lightbulb, Scale} from 'lucide-react'
 
 function WhoWeAre() {
 
@@ -8,7 +8,9 @@ function WhoWeAre() {
         {title: "Our Mission & Purpose", icon: Compass, border_icon_color: "#5BA4B5", text: "some text here"},
         {title: "Vision For The Future", icon: Telescope, border_icon_color: "#E8A562", text: "some more text"},
         {title: 'Unique Strengths & Capabilities', icon: Award, border_icon_color: "#C4D65E", text: "yet more text..."},
-        {title: "Measuring Success", icon: Flag, border_icon_color: "#5BA4B5", text: "more text,,,"}
+        {title: "Measuring Success", icon: Flag, border_icon_color: "#5BA4B5", text: "more text,,,"},
+        {title: "Adaptation & Innovation", icon: Lightbulb, border_icon_color: "#E8A562", text: "bleh bleh bleh"},
+        {title: "Ethical Considerations", icon: Scale, border_icon_color: "#C4D65E", text: "again again again"}
     ]
    
     return (
