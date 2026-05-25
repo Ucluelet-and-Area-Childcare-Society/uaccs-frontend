3192d8585b1b0ed11217bedd16275fb113cdf428
rohan-projects01 <rohan01.projects@gmail.com>
2026-05-25 03:33:15 -0700

    updated array to be export const

--- uaccs/src/data/TimelineItems.js
+++ uaccs/src/data/TimelineItems.js
@@ -1,7 +1,7 @@
 import {Compass, Telescope, Award, Flag, Lightbulb, Scale} from 'lucide-react'
 
 /* Array of timeline items to display on Who We Are section */
-const TimelineItems = [
+export const TimelineItems = [
         {title: "Our Mission & Purpose", icon: Compass, border_icon_color: "#5BA4B5", text: "some text here"},
         {title: "Vision For The Future", icon: Telescope, border_icon_color: "#E8A562", text: "some more text"},
         {title: 'Unique Strengths & Capabilities', icon: Award, border_icon_color: "#C4D65E", text: "yet more text..."},
