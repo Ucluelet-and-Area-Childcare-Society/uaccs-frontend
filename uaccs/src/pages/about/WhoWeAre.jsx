23931b67fb0c90bff9b2d0d4d8c8162daed4652a
rohan-projects01 <rohan01.projects@gmail.com>
2026-05-25 02:31:37 -0700

    added imports for new timeline libary

--- uaccs/src/pages/about/WhoWeAre.jsx
+++ uaccs/src/pages/about/WhoWeAre.jsx
@@ -1,4 +1,6 @@
 import Timeline from '../../components/Timeline'
+import {VerticalTimeline, VerticalTimelineElement} from 'react-vertical-timeline-component'
+import 'react-vertical-timeline-component/style.min.css'
 
 function WhoWeAre() {
 
