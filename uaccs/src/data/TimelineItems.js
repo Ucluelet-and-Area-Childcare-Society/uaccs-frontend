719927c0f44d52bbe1b90b733e9c2e578a2dbc4c
rohan-projects01 <rohan01.projects@gmail.com>
2026-05-25 03:44:05 -0700

    updated timelineitems text info

--- uaccs/src/data/TimelineItems.js
+++ uaccs/src/data/TimelineItems.js
@@ -6,36 +6,63 @@ export const TimelineItems = [
             title: "Our Mission & Purpose", 
             icon: Compass, 
             border_icon_color: "#5BA4B5", 
-            text: "some text here"
+            text: ` UACCS is committed to providing affordable, inclusive, 
+            safe, and nurturing childcare for children in the Ucluelet area. 
+            Founded on the belief that every child deserves quality care, 
+            we create a supportive community where parents can pursue their livelihoods with peace of mind. 
+            By exceeding licensing requirements and fostering early learning skills, 
+            we prioritize each child's well-being and development.`
         },
         {
             title: "Vision For The Future", 
             icon: Telescope, 
             border_icon_color: "#E8A562", 
-            text: "some more text"
+            text: ` Looking ahead, our long-term vision for UACCS is one of growth,
+            empowerment, and community cohesion. We aspire to reduce waitlists and expand our capacity,
+            ensuring that no family in Ucluelet is left without access to the childcare services they need. 
+            Beyond mere provision, we envision UACCS as a cornerstone of our community, a place where families come together 
+            to forge lasting connections, share experiences, and support one another. By creating a welcoming and safe space,
+            we aim to uplift families, enhance the well-being of children and parents, and foster a sense of belonging that resonates
+            throughout our coastal town.`
         },
         {
             title: 'Unique Strengths & Capabilities', 
             icon: Award, 
             border_icon_color: "#C4D65E", 
-            text: "yet more text..."
+            text: `What sets UACCS apart is our unwavering commitment to our mission and the unique role we play as the only not-for-profit 
+            society on the West Coast providing childcare services. Leveraging the collective experience, expertise,
+            and community connections of our dedicated staff, board members, and parents, we harness the power of collaboration to 
+            maximize our impact and deliver unparalleled care and support to Ucluelet families. `
         },
         {
             title: "Measuring Success", 
             icon: Flag, 
             border_icon_color: "#5BA4B5", 
-            text: "more text,,,"
+            text: `Success for UACCS is measured not only in numbers but also in the smiles of happy, 
+            safe, and healthy children and the sense of support and encouragement felt by families across 
+            our community. By tracking the number of children in our care and on our waitlist, we strive to grow 
+            and expand our reach in line with the demands of our community. `
         },
         {
             title: "Adaptation & Innovation", 
             icon: Lightbulb, 
             border_icon_color: "#E8A562", 
-            text: "bleh bleh bleh"
+            text: `In the face of evolving community needs and changing circumstances, 
+            UACCS remains steadfast in its commitment to innovation and adaptation.  
+            While challenges such as staffing shortages and accommodation may pose obstacles, 
+            we embrace the opportunity to invest in training and development, ensuring that our team is equipped 
+            to meet the needs of the families we serve. With a progressive board at the helm, we are open to fresh ideas 
+            and approaches, dedicated to keeping UACCS relevant and responsive to the needs of our ever-changing community. `
         },
         {
             title: "Ethical Considerations", 
             icon: Scale, 
             border_icon_color: "#C4D65E", 
-            text: "again again again"
+            text: `Ethical behaviour lies at the heart of 
+            everything we do at UACCS, bound to the principles
+            and regulations set forth by the Community Care and Assisted Living Act Child Care
+            Licensing Regulations. Our goal is to not only meet but also exceed these standards, 
+            particularly concerning staffing ratios, ensuring that every child receives the care and 
+            attention they deserve in a safe and nurturing environment. `
         }
     ]
