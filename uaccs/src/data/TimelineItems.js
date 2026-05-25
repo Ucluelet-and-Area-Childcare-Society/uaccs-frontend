44cef20236bf29c738f982ebddc46c5e7f9beda0
rohan-projects01 <rohan01.projects@gmail.com>
2026-05-25 03:35:06 -0700

    updated array structure for cleaner format

--- uaccs/src/data/TimelineItems.js
+++ uaccs/src/data/TimelineItems.js
@@ -2,10 +2,40 @@ import {Compass, Telescope, Award, Flag, Lightbulb, Scale} from 'lucide-react'
 
 /* Array of timeline items to display on Who We Are section */
 export const TimelineItems = [
-        {title: "Our Mission & Purpose", icon: Compass, border_icon_color: "#5BA4B5", text: "some text here"},
-        {title: "Vision For The Future", icon: Telescope, border_icon_color: "#E8A562", text: "some more text"},
-        {title: 'Unique Strengths & Capabilities', icon: Award, border_icon_color: "#C4D65E", text: "yet more text..."},
-        {title: "Measuring Success", icon: Flag, border_icon_color: "#5BA4B5", text: "more text,,,"},
-        {title: "Adaptation & Innovation", icon: Lightbulb, border_icon_color: "#E8A562", text: "bleh bleh bleh"},
-        {title: "Ethical Considerations", icon: Scale, border_icon_color: "#C4D65E", text: "again again again"}
+        {
+            title: "Our Mission & Purpose", 
+            icon: Compass, 
+            border_icon_color: "#5BA4B5", 
+            text: "some text here"
+        },
+        {
+            title: "Vision For The Future", 
+            icon: Telescope, 
+            border_icon_color: "#E8A562", 
+            text: "some more text"
+        },
+        {
+            title: 'Unique Strengths & Capabilities', 
+            icon: Award, 
+            border_icon_color: "#C4D65E", 
+            text: "yet more text..."
+        },
+        {
+            title: "Measuring Success", 
+            icon: Flag, 
+            border_icon_color: "#5BA4B5", 
+            text: "more text,,,"
+        },
+        {
+            title: "Adaptation & Innovation", 
+            icon: Lightbulb, 
+            border_icon_color: "#E8A562", 
+            text: "bleh bleh bleh"
+        },
+        {
+            title: "Ethical Considerations", 
+            icon: Scale, 
+            border_icon_color: "#C4D65E", 
+            text: "again again again"
+        }
     ]
