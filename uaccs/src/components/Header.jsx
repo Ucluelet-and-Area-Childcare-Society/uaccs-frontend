4de969b5a532c92ef4130b40a90ed083fcf993cc
rohan-projects01 <rohan01.projects@gmail.com>
2026-05-28 22:09:49 -0700

    updated logo styling

--- uaccs/src/components/Header.jsx
+++ uaccs/src/components/Header.jsx
@@ -20,17 +20,8 @@ function Header() {
 
     return (
         <header className = "relative z-20 w-full px-6 py-4 flex flex-row bg-uaccs_blue justify-between">
-            <div className='flex items-center gap-3 select-none'>
-                <img src = {Umbrella} alt = "Umbrella Logo" className= "w-16 h-16 "/>
-                <div className='tracking-wider'>
-                    <div className='text-[18px] text-white font-bold'>
-                        UCLUELET &amp; AREA
-                    </div>
-                    <div className='text-[11px] text-uaccs_green font-semibold'>
-                        CHILDCARE SOCIETY
-                    </div>
-                </div>
-            </div>
+            <img src = {Umbrella} alt = "Umbrella Logo" className= "w-16 h-16 "/>
+
             <nav className = "flex items-center gap-7 text-white font-schoolbell text-md">
                 <DropdownMenu label = "About" items = {aboutLinks} />
                 {navLinks.map((link) => (
@@ -38,6 +29,7 @@ function Header() {
                 ))}
                 <NavLink to = "/waitlist" className = 'bg-uaccs_orange p-2 rounded-md hover:text-uaccs_orange hover:bg-white transition-all duration-300 ease-in-out hover:-translate-y-1'> Waitlist </NavLink>
             </nav>
+            
             <div className = "absolute bottom-0 left-0 w-full overflow-hidden translate-y-full">
                 <svg
                 viewBox="0 0 1440 320"
