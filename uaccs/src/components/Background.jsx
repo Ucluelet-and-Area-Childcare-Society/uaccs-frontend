d8748b76ec53f4faf18d60a6cc8a8e98fc59fb3e
rohan-projects01 <rohan01.projects@gmail.com>
2026-05-29 04:32:34 -0700

    added logic to swap direction upon hitting boundary

--- uaccs/src/components/Background.jsx
+++ uaccs/src/components/Background.jsx
@@ -7,14 +7,15 @@ function Background() {
     const itemsRef = useRef([])
     const valueRef = useRef()
     const NUM_ELEMENTS = 100
+    const boundary = 50
 
 
     // trigger render only once
     useEffect(() => {
         itemsRef.current = Array.from({length : NUM_ELEMENTS}).map(() => ({
             elem: null,
-            x: Math.random() * (window.innerWidth - 50),
-            y: Math.random() * (window.innerHeight - 50),
+            x: Math.random() * (window.innerWidth - boundary),
+            y: Math.random() * (window.innerHeight - boundary),
             dx: (Math.random() > 0.5 ? 1 : -1),
             dy: (Math.random() > 0.5 ? 1 : -1),
         }))
@@ -22,14 +23,20 @@ function Background() {
 
     const animate = () => {
         itemsRef.current.forEach((item) => {
-            x += dx
-            y += dy
-            
+            item.x += item.dx
+            item.y += item.dy
+
+            // swap direction if exceeds bounds
+            if (item.x <= 0 || item.x + boundary >= window.innerWidth) {
+                item.dx *= -1
+
+            }
+
+            if (item.y <= 0 || item.y + boundary >= window.innerHeight) {
+                item.dy *= -1
+
+            }  
         });
-
-    
-
-
 
         valueRef.current = requestAnimationFrame(animate)
     }
