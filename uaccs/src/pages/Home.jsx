4323a7e458cb7240e95efb751b0aca490a00e9e5
rohan-projects01 <rohan01.projects@gmail.com>
2026-05-29 01:21:59 -0700

    updated image styling to look like a cutout

--- uaccs/src/pages/Home.jsx
+++ uaccs/src/pages/Home.jsx
@@ -38,14 +38,18 @@ function Home() {
         </h1>
         <div className = "flex flex-row mt-20 gap-30 justify-center">
 
-            <div className = "rounded-[60%_40%_30%_70%/60%_30%_70%_40%] overflow-hidden shadow-md">
-                <img 
-                src = "/test_img.jpg" 
-                alt = "test image" 
-                width = "550" 
-                height = "550" 
-                className = "pointer-events-none object-cover"
-                />
+            <div className = "inset-0 rounded-[60%_40%_30%_70%/60%_30%_70%_40%] bg-white border-6 border-dashed border-uaccs_green">
+                <div className = "rounded-[60%_40%_30%_70%/60%_30%_70%_40%] overflow-hidden shadow-lg">
+                    <img 
+                        src = "/test_img.jpg" 
+                        alt = "test image" 
+                        width = "550" 
+                        height = "550" 
+                        className = "pointer-events-none object-cover"
+                    />
+                </div>
+
+
             </div>
 
 
