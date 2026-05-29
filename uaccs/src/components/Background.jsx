50b8cb74e318a99dfd79e16f11f13519513da171
rohan-projects01 <rohan01.projects@gmail.com>
2026-05-29 04:42:01 -0700

    restored to default

--- uaccs/src/components/Background.jsx
+++ uaccs/src/components/Background.jsx
@@ -1,55 +1,62 @@
 import {Square, Circle, Triangle} from 'lucide-react'
-import { useRef, useEffect } from 'react';
+import {motion} from 'motion/react'
+
+/* Returns a div with the specified coordinates and layout*/
+function createWatermark(key, x, y, rotation, color, Icon) {
+    return (
+        <div
+        key = {key}
+        className = "absolute pointer-events-none overflow-hidden z-0"
+        style = {{
+            color: color,
+            opacity: 0.20,
+            position: 'absolute',
+            top: `${y}%`,
+            left: `${x}%`,
+            transform: `rotate(${rotation}deg)`,
+            whiteSpace: "nowrap",
+        }}
+        >
+            <Icon size = {48} strokeWidth = {1.5} fill = {color}/>
+        </div>
+
+    );
+
+}
 
 
 /* Background component for randomly layed out phrase as a watermark */
 function Background() {
-    const itemsRef = useRef([])
-    const valueRef = useRef()
-    const NUM_ELEMENTS = 100
-    const boundary = 50
+    const elements = []
+    const colors = ['#5BA4B5', '#C4D65E', '#E8A562']
+    const icons = [Square, Circle, Triangle]
+    const rows = 10
+    const cols = 6
+    let key = 0
 
+    // randomize element position within its own grid cell to reduce overlap
+    for (let i = 0; i < rows; i++) {
+        for (let j = 0; j < cols; j++) {
+            const xStart = (j / cols) * 100
+            const yStart = (i / rows) * 100
 
-    // trigger render only once
-    useEffect(() => {
-        itemsRef.current = Array.from({length : NUM_ELEMENTS}).map(() => ({
-            elem: null,
-            x: Math.random() * (window.innerWidth - boundary),
-            y: Math.random() * (window.innerHeight - boundary),
-            dx: (Math.random() > 0.5 ? 1 : -1),
-            dy: (Math.random() > 0.5 ? 1 : -1),
-        }))
-    }, [])
+            // x and y coordinate should be within its own grid position
+            const x = xStart + (Math.random() * (100 / cols))
+            const y = yStart + (Math.random() * (100 / rows))
+            const rotation = Math.floor((Math.random() * 90) - 45) // -45 to 45 degrees;
+            const color = colors[Math.floor(Math.random() * colors.length)]
+            const icon = icons[Math.floor(Math.random() * icons.length)]
 
-    const animate = () => {
-        itemsRef.current.forEach((item) => {
-            item.x += item.dx
-            item.y += item.dy
+            elements.push(createWatermark(key, x, y, rotation, color, icon))
+            key++
+        }
 
-            // swap direction if exceeds bounds
-            if (item.x <= 0 || item.x + boundary >= window.innerWidth) {
-                item.dx *= -1
-
-            }
-
-            if (item.y <= 0 || item.y + boundary >= window.innerHeight) {
-                item.dy *= -1
-
-            }  
-        });
-
-        valueRef.current = requestAnimationFrame(animate)
     }
-
-    useEffect(() => {
-        valueRef.current = requestAnimationFrame(animate) // start the animation
-        return () => cancelAnimationFrame(valueRef.current)
-    }, [])
 
 
     return (
         <div className = "fixed inset-0 pointer-events-none z-0 bg-[#F5F9FA] overflow-hidden ">
-            
+            {elements}
         </div>
     );
 
