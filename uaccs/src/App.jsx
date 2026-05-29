d4278d5042f657777387aaf050e6590ce10f99a5
rohan-projects01 <rohan01.projects@gmail.com>
2026-05-28 20:47:07 -0700

    updated footer scroll positioning

--- uaccs/src/App.jsx
+++ uaccs/src/App.jsx
@@ -37,8 +37,8 @@ function App() {
             <Route path = "/get-involved" element = {<GetInvolved/>}/>
             <Route path = "/resources" element = {<Resources/>}/>
         </Routes>
+        <Footer/>
     </main>
-    <Footer/>
    </div>
   )
 }
