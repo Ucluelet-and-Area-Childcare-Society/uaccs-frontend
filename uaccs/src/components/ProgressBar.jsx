2ab811959a29c2cc5974f0517f3355128870fbfb
rohan-projects01 <rohan01.projects@gmail.com>
2026-06-01 23:24:19 -0700

    added imports for react hooks and stub for component function return

--- uaccs/src/components/ProgressBar.jsx
+++ uaccs/src/components/ProgressBar.jsx
@@ -1,2 +1,14 @@
 import {VerticalTimeline, VerticalTimelineElement} from 'react-vertical-timeline-component'
 import 'react-vertical-timeline-component/style.min.css'
+import {useEffect, useState, useRef} from 'react'
+
+function ProgressBar() {
+
+    return (
+        <>
+        </>
+    )
+
+}
+
+export default ProgressBar;
