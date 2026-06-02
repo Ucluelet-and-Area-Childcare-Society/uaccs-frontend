6bc10ace766e2e3fcaf3a632ef869566545ad444
rohan-projects01 <rohan01.projects@gmail.com>
2026-06-01 23:46:03 -0700

    added comments, removed imports and defined new array to map progress bar positions for fill animation

--- uaccs/src/components/ProgressBar.jsx
+++ uaccs/src/components/ProgressBar.jsx
@@ -1,7 +1,15 @@
-import {VerticalTimeline, VerticalTimelineElement} from 'react-vertical-timeline-component'
-import 'react-vertical-timeline-component/style.min.css'
-import {useEffect, useState, useRef} from 'react'
 
+
+const steps = [
+    {id: 1, title: "Our Mission & Purpose"},
+    {id: 2, title: "Vision for the Future"},
+    {id: 3, title: "Unique Strengths & Capabilities"},
+    {id: 3, title: "Measuring Success"},
+    {id: 4, title: "Adaptation & Innovation"},
+    {id: 5, title: "Ethical Considerations"}
+]
+
+/* Progress bar component to showcase current navigation position */
 function ProgressBar() {
 
     return (
