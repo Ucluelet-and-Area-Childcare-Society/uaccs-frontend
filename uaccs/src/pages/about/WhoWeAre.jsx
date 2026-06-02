cada8f542f197ec7a8c3525378dd6ee485c96aef
rohan-projects01 <rohan01.projects@gmail.com>
2026-06-01 23:22:56 -0700

    removed who we are return to make updates

--- uaccs/src/pages/about/WhoWeAre.jsx
+++ uaccs/src/pages/about/WhoWeAre.jsx
@@ -1,41 +1,10 @@
-import {VerticalTimeline, VerticalTimelineElement} from 'react-vertical-timeline-component'
-import 'react-vertical-timeline-component/style.min.css'
-import {TimelineItems} from '../../data/TimelineItems'
 
 
 function WhoWeAre() {
 
     return (
-        <div className = "flex flex-col items-center p-2 mx-auto z-0 w-full">
-            <h1 className = "font-dancing text-[#A8BA4E] text-[100px]">Who We Are</h1>
-            <VerticalTimeline lineColor='#5BA4B5'>
-                {TimelineItems.map((item, index) => (
-                    <VerticalTimelineElement
-                    key = {index}
-                    contentStyle = {{
-                        border: `8px double ${item.border_icon_color}`,
-                    }}
-                    contentArrowStyle={{
-                    display: 'none'
-                    }}
-                    icon = {<item.icon />}
-                    iconStyle={{
-                    background: '#FFFFFF',
-                    color: `${item.border_icon_color}`,
-                    }}
-                    className = "transition-all duration-300 ease-in-out hover:-translate-y-2 hover:scale-[1.10]"
-                    >
-                        <h3 className = "vertical-timeline-element-title font-dancing text-3xl text-semi mb-10!">
-                            {item.title}
-                        </h3>
-                        <p className = "text-justify text-gray-700">
-                            {item.text}
-                        </p>
-
-                    </VerticalTimelineElement>
-                ))}
-            </VerticalTimeline> 
-        </div>
+        <>
+        </>
     )
 
 }
