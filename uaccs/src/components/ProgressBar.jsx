78057dedd64ff4de25feaad563c2cf93d440bb47
rohan-projects01 <rohan01.projects@gmail.com>
2026-06-02 01:28:09 -0700

    added scrollYProgress as a param to Step to enable circle fill animation on scroll

--- uaccs/src/components/ProgressBar.jsx
+++ uaccs/src/components/ProgressBar.jsx
@@ -47,11 +47,11 @@ function ProgressBar() {
 
 }
 
-function Step({step}) {
+function Step({step, scrollYProgress}) {
     return (
         <div className = "flex items-center gap-4">
             <div className = "relative w-5 h-5 rounded-full border-2 border-gray-300 shrink-0 bg-white"/>
-            <div className = "text-sm text-gray-700 whitespace-nowrap">{step.title}</div>
+            <div className = "text-sm text-gray-500 whitespace-nowrap">{step.title}</div>
         </div>
     )
 
