d97bc8ec526791f83884b46e8e8e9d66f5c2ca8d
rohan-projects01 <rohan01.projects@gmail.com>
2026-06-02 23:40:27 -0700

    added in previous card logic to abstract it out

--- uaccs/src/components/CardStrip.jsx
+++ uaccs/src/components/CardStrip.jsx
@@ -1,6 +1,26 @@
 
 function CardStrip({items}) {
 
+    return (
+        <div className = "grid grid-cols-3 grid-rows-1 max-w-6xl gap-15 w-full mt-20 text-center font-dancing text-2xl">
+          {items.map((item, index) => {
+
+            const Icon = program.icon
+            return (
+                <div 
+                key = {index} 
+                className = {`bg-white h-64 border-6 py-5 shadow-custom_shadow flex flex-col items-center justify-between ${program.borderColor} ${program.textColor} ${program.shadowColor}`}>
+                    <h3 className = "font-uaccs text-3xl" >{item.title}</h3>
+                    <Icon size = {36} />
+                    <div className = "text-gray-700 font-extralight text-[18px] font-schoolbell">
+                        {program.ages}
+                    </div>
+                </div>
+            )
+          })}  
+        </div>
+    )
+
 }
 
 export default CardStrip;
