69051829982a776ed87cdbf3491d9869d88d93ce
rohan-projects01 <rohan01.projects@gmail.com>
2026-06-06 22:03:43 -0700

    updated background styling of new section on home page

--- uaccs/src/pages/Home.jsx
+++ uaccs/src/pages/Home.jsx
@@ -95,43 +95,44 @@ function Home() {
             </div>
 
 
-
-            <div className = "flex flex-col items-center p-10 mx-auto z-0 max-w-7xl" >
-                <section className = "mt-20 rounded-lg">
-                    <h2 className = "text-4xl font-nunito font-bold text-uaccs_blue text-center mb-10">Why Parents Choose Us</h2>
-                    <div className = "flex flex-row items-center gap-15">
-                        <div className = "flex flex-col gap-10 w-1/2">
-                            <div className = "mt-5 px-6 py-3 rounded-2xl text-2xl font-black font-nunito bg-uaccs_orange text-white w-fit">
-                                💰 Just $10/day!
+            <div className = "relative w-full overflow-hidden bg-[#D4E8EC]">
+                <div className = "flex flex-col items-center p-10 mx-auto z-0 max-w-7xl" >
+                    <section className = "mt-20 rounded-lg">
+                        <h2 className = "text-4xl font-nunito font-bold text-uaccs_blue text-center mb-10">Why Parents Choose Us</h2>
+                        <div className = "flex flex-row items-center gap-15">
+                            <div className = "flex flex-col gap-10 w-1/2">
+                                <div className = "mt-5 px-6 py-3 rounded-2xl text-2xl font-black font-nunito bg-uaccs_orange text-white w-fit">
+                                    💰 Just $10/day!
+                                </div>
+                                <h3 className = "font-nunito font-bold text-2xl text-[#264D57]">
+                                    Affordable, Inclusive, Safe, and Nurturing childcare for every family
+                                </h3>
+                                <p className = "text-justify leading-relaxed">
+                                    At UACCS, we believe every child deserves a nurturing, 
+                                    high-quality early education — regardless of family income. 
+                                    Our $10/day BC Affordable Child Care Benefit rate means you never have 
+                                    to choose between work and your child's wellbeing.
+                                </p>
+                                <p className = "text-justify leading-relaxed">
+                                    It is our privilege and honour to play a part 
+                                    in shaping a brighter future for the next generation of 
+                                    Ucluelet residents. 
+                                </p>
+                                <NavLink to = "/about/who-we-are" className = "flex flex-row gap-1 bg-uaccs_blue px-6 py-4 rounded-full text-white font-nunito duration-200 transition-all hover:shadow-xl hover:translate-x-1 hover:-translate-y-1 w-fit">
+                                    Learn More
+                                    <ArrowRight/>
+                                </NavLink>
                             </div>
-                            <h3 className = "font-nunito font-bold text-2xl text-[#264D57]">
-                                Affordable, Inclusive, Safe, and Nurturing childcare for every family
-                            </h3>
-                            <p className = "text-justify leading-relaxed">
-                                At UACCS, we believe every child deserves a nurturing, 
-                                high-quality early education — regardless of family income. 
-                                Our $10/day BC Affordable Child Care Benefit rate means you never have 
-                                to choose between work and your child's wellbeing.
-                            </p>
-                            <p className = "text-justify leading-relaxed">
-                                It is our privilege and honour to play a part 
-                                in shaping a brighter future for the next generation of 
-                                Ucluelet residents. 
-                            </p>
-                            <NavLink to = "/about/who-we-are" className = "flex flex-row gap-1 bg-uaccs_blue px-6 py-4 rounded-full text-white font-nunito duration-200 transition-all hover:shadow-xl hover:translate-x-1 hover:-translate-y-1 w-fit">
-                                Learn More
-                                <ArrowRight/>
-                            </NavLink>
+                            <div className = "w-1/2">
+                                <img 
+                                    src = "/test_img.jpg" 
+                                    alt = "test image" 
+                                    className = "pointer-events-none select-none h-100 rounded-lg object-cover"
+                                />
+                            </div>  
                         </div>
-                        <div className = "w-1/2">
-                            <img 
-                                src = "/test_img.jpg" 
-                                alt = "test image" 
-                                className = "pointer-events-none select-none h-100 rounded-lg object-cover"
-                            />
-                        </div>  
-                    </div>
-                </section>
+                    </section>
+                </div>
             </div>
         </>
     )
